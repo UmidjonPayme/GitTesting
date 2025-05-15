1d610710399d242321c618297a0fe211b6a228ae
UmidjonPayme <umidjon.f@payme.uz>
2025-05-15 17:11:16 +0500

    Initial Commit

---
+++ GitTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTesting
+//
+//  Created by Umidjon on 15/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
